ee471037ddba84abc5f2eb08e396d4f003c7c1cf
manojvenp <manojkumark@venpep.com>
2025-01-11 20:55:40 +0530

    Update aiagent.py

--- aiagent.py
+++ aiagent.py
@@ -1,4 +1,5 @@
 from langflow.load import run_flow_from_json
+
 TWEAKS = {
   "ChatInput-SwYLi": {},
   "CSVAgent-8XYIT": {},
@@ -7,8 +8,12 @@ TWEAKS = {
   "CSVAgent-LnypR": {}
 }
 
-result = run_flow_from_json(flow="Doorsharp AI Agent (CSV) (1).json",
-                            input_value="message",
-                            session_id="", # provide a session id if you want to use session state
-                            fallback_to_env_vars=True, # False by default
-                            tweaks=TWEAKS)
+result = run_flow_from_json(
+    flow="Doorsharp AI Agent (CSV) (1).json",
+    input_value="message",
+    session_id="",  # provide a session id if you want to use session state
+    fallback_to_env_vars=True,  # False by default
+    tweaks=TWEAKS
+)
+
+print(result)
